5312eefd99df432063edf0fa36ca14c37c51db31
Wadi Wahyudin <wadi.wahyudin7@gmail.com>
2024-02-23 16:27:23 +0700

    create a dynamic navbar with array

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,12 +4,24 @@ import Link from 'next/link'
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+const links = [
+    { label : 'Dashboard', href: '/dashboard'},
+    { label : 'Issues', href: '/issues'},
+]
+
+
   return (
      <nav className='flex space-x-6 border-b px-5 mb-5 h-14 items-center'>
         <Link href='/'><FaBug /></Link>
         <ul className='flex space-x-6'>
-            <li><Link href='/dashboard'>Dashboard</Link></li>
-            <li><Link href='/issues'>Issues</Link></li>
+            {links.map((link) => (
+                <Link 
+                key={link.href} 
+                className='text-zinc-500 hover:text-zinc-800 transition-color' 
+                href={link.href}>{link.label}</Link>
+            ))}
+            <li></li>
+            <li></li>
         </ul>
      </nav>
     )
